cdd054063cbb80f163fd565eb7fdb33ebd23c838
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-03-12 14:02:32 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.cfbf8b8b943b1255b4d76feaf5036d14.js"
 );
 
 self.addEventListener('message', (event) => {
